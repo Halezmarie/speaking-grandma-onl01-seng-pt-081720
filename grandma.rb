75b4ab525cd6f6fef5be78968a7715fb8550d455
Haley Grassi <teacherhaleymarie@gmail.com>
2020-08-24 22:54:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-  elseif phrase != phrase.uppercase
+  elseif phrase != phrase.upcase
     "HUH?! SPEAK UP, SONNY!"
 end 
 end
