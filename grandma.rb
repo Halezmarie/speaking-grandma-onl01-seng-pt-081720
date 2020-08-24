51dda11ac83a585dbba8c7e9b7d0d7a06e71a02d
Haley Grassi <teacherhaleymarie@gmail.com>
2020-08-24 22:45:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,9 @@ def speak_to_grandma(phrase)
 				        "I LOVE YOU TOO PUMPKIN"
 				 elsif phrase != phrase.upcase
 				        "HUH!? SPEAK UP SONNY!"
-				 else phrase == phrase.upcase #if our argument of phrase is EQUAL TO phrase.upcase (uppercase) then its true!
-				        "NO, NOT SINCE 1938!"      #so grandma responds....
-				end
+				 else phrase == phrase.upcase
+				   
+				   #if our argument of phrase is EQUAL TO phrase.upcase (uppercase) then its true!
+				        "NO, NOT SINCE 1938!"    
+				        #so grandma responds....
 end 
